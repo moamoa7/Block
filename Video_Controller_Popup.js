eb6807a69f44f440856fa646ec803dacbc71b76f
moamoa7 <chandung@nate.com>
2025-07-10 01:45:03 +0900

    Update Video_Controller_Popup.js

--- Video_Controller_Popup.js
+++ Video_Controller_Popup.js
@@ -64,7 +64,7 @@
     popup.style.transition = 'opacity 0.3s ease';
 
     // ✅ PC/모바일 분기
-    popup.style.opacity = isMobile ? '0.125' : '0.2';
+    popup.style.opacity = isMobile ? '0.1' : '0.2';
 
     // 영상 선택 셀렉트 박스
     const select = document.createElement('select');
@@ -148,7 +148,7 @@
         popup.style.opacity = '1';
       });
       popup.addEventListener('mouseleave', () => {
-        popup.style.opacity = '0.05';
+        popup.style.opacity = '0.01';
       });
     }
 
