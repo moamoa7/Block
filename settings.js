ad54c229d8e56fbc5cc6c873f09c0b2f5a402ff6
moamoa7 <chandung@nate.com>
2025-08-25 16:06:17 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@
 
     const CONFIG = {
         DEFAULT_VIDEO_FILTER_LEVEL: isMobile ? 4 : 2,
-        DEFAULT_IMAGE_FILTER_LEVEL: isMobile ? 3 : 2,
+        DEFAULT_IMAGE_FILTER_LEVEL: isMobile ? 4 : 2,
         DEFAULT_AUDIO_PRESET: 'off',
         DEBUG: false,
         DEBOUNCE_DELAY: 300,
