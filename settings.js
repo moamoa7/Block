76938f4e5d5231cf9bbe8aaad3705947be41828e
moamoa7 <chandung@nate.com>
2026-01-31 08:51:31 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -847,7 +847,7 @@
             videoXXBrightenBtn.className = 'vsc-btn';
             videoXXBrightenBtn.textContent = 'XL';
             videoXXBrightenBtn.dataset.presetKey = 'brightenX4';
-            videoXXBrightenBtn.onclick = () => { this.stateManager.set('videoFilter.gamma', 2.05); this.stateManager.set('videoFilter.saturation', 112); this.stateManager.set('videoFilter.blur', 0); this.stateManager.set('videoFilter.shadows', -27); this.stateManager.set('videoFilter.highlights', 15); this.stateManager.set('videoFilter.activePreset', 'brightenX4'); };
+            videoXXBrightenBtn.onclick = () => { this.stateManager.set('videoFilter.gamma', 2.15); this.stateManager.set('videoFilter.saturation', 112); this.stateManager.set('videoFilter.blur', 0); this.stateManager.set('videoFilter.shadows', -27); this.stateManager.set('videoFilter.highlights', 15); this.stateManager.set('videoFilter.activePreset', 'brightenX4'); };
 
             const videoButtonsContainer = document.createElement('div');
             videoButtonsContainer.style.cssText = 'display: flex; flex-direction: column; gap: 0px; margin-top: 0px; width: 100%;';
