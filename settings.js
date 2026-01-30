84da791fb79d3a8fc784584a75e03b5e225cc346
moamoa7 <chandung@nate.com>
2026-01-31 00:08:39 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -841,13 +841,13 @@
             videoXLBrightenBtn.className = 'vsc-btn';
             videoXLBrightenBtn.textContent = 'L';
             videoXLBrightenBtn.dataset.presetKey = 'brightenX3';
-            videoXLBrightenBtn.onclick = () => { this.stateManager.set('videoFilter.gamma', 1.90); this.stateManager.set('videoFilter.saturation', 106); this.stateManager.set('videoFilter.blur', 0); this.stateManager.set('videoFilter.shadows', -40); this.stateManager.set('videoFilter.highlights', 20); this.stateManager.set('videoFilter.activePreset', 'brightenX3'); };
+            videoXLBrightenBtn.onclick = () => { this.stateManager.set('videoFilter.gamma', 1.80); this.stateManager.set('videoFilter.saturation', 106); this.stateManager.set('videoFilter.blur', 0); this.stateManager.set('videoFilter.shadows', -40); this.stateManager.set('videoFilter.highlights', 20); this.stateManager.set('videoFilter.activePreset', 'brightenX3'); };
 
             const videoXXBrightenBtn = document.createElement('button');
             videoXXBrightenBtn.className = 'vsc-btn';
             videoXXBrightenBtn.textContent = 'XL';
             videoXXBrightenBtn.dataset.presetKey = 'brightenX4';
-            videoXXBrightenBtn.onclick = () => { this.stateManager.set('videoFilter.gamma', 2.30); this.stateManager.set('videoFilter.saturation', 108); this.stateManager.set('videoFilter.blur', 0); this.stateManager.set('videoFilter.shadows', -60); this.stateManager.set('videoFilter.highlights', 30); this.stateManager.set('videoFilter.activePreset', 'brightenX4'); };
+            videoXXBrightenBtn.onclick = () => { this.stateManager.set('videoFilter.gamma', 2.10); this.stateManager.set('videoFilter.saturation', 108); this.stateManager.set('videoFilter.blur', 0); this.stateManager.set('videoFilter.shadows', -60); this.stateManager.set('videoFilter.highlights', 30); this.stateManager.set('videoFilter.activePreset', 'brightenX4'); };
 
             const videoButtonsContainer = document.createElement('div');
             videoButtonsContainer.style.cssText = 'display: flex; flex-direction: column; gap: 0px; margin-top: 0px; width: 100%;';
