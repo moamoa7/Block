b2acbdb3050140cb02802e3cc66b61e4cd99f632
moamoa7 <chandung@nate.com>
2026-02-26 02:34:30 +0900

    Update Video_Control.js

--- Video_Control.js
+++ Video_Control.js
@@ -95,7 +95,7 @@
     });
 
     const PRESETS = Object.freeze({
-      detail: { off: { sharpAdd: 0, sharp2Add: 0, clarityAdd: 0 }, S: { sharpAdd: 6, sharp2Add: 4, clarityAdd: 2 }, M: { sharpAdd: 12, sharp2Add: 8, clarityAdd: 5 }, L: { sharpAdd: 19, sharp2Add: 11, clarityAdd: 8 }, XL: { sharpAdd: 25, sharp2Add: 15, clarityAdd: 10} },
+      detail: { off: { sharpAdd: 0, sharp2Add: 0, clarityAdd: 0 }, S: { sharpAdd: 6, sharp2Add: 4, clarityAdd: 2 }, M: { sharpAdd: 12, sharp2Add: 8, clarityAdd: 5 }, L: { sharpAdd: 19, sharp2Add: 11, clarityAdd: 8}, XL: { sharpAdd: 25, sharp2Add: 15, clarityAdd: 10 } },
       grade: {
         brOFF: { gammaF: 1.00, brightAdd: 0 },
         S:     { gammaF: 1.02, brightAdd: 1.8 },
