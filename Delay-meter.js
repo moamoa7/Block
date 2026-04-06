b043ceffb0aec73862b1110b15d2c087f16f8d31
moamoa7 <chandung@nate.com>
2026-04-06 16:49:19 +0900

    Update version number to 16.1.4

--- Delay-meter.js
+++ Delay-meter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         딜레이 자동 제어
 // @namespace    https://github.com/moamoa7
-// @version      15.0.2
+// @version      16.1.4
 // @description  라이브 방송의 딜레이를 자동 감지·제어 (경량화)
 // @author       DelayMeter
 // @match        *://*.youtube.com/*
