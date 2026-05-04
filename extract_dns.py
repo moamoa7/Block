7c102eaddcdc167ce58e4a43b6ff3bc9c479b844
moamoa7 <chandung@nate.com>
2026-05-04 18:43:04 +0900

    Update extract_dns.py

--- extract_dns.py
+++ extract_dns.py
@@ -12,6 +12,7 @@ FILTER_URLS = [
     "https://filters.adtidy.org/windows/filters/7.txt",
     "https://cdn.jsdelivr.net/npm/@list-kr/filterslists@latest/dist/filterslist-AdGuard-classic.txt",
     "https://ublockorigin.github.io/uAssets/filters/filters.txt",
+    "https://raw.githubusercontent.com/moamoa7/adblock/main/block.txt",
 ]
 EXCLUSION_URLS = [
     "https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt",
@@ -50,6 +51,8 @@ def short_name(url: str) -> str:
         return "List-KR"
     if "uAssets" in url:
         return "uBlock Filters"
+    if "main/block.txt" in url:
+        return "Personal Blocklist"
     if "exclusions.txt" in url:
         return "AdGuard DNS Exclusions"
     if "white.txt" in url:
