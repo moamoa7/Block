65a8d43a4a25a20ef79adb97c4a5a9350cd1a3d3
moamoa7 <chandung@nate.com>
2026-05-07 17:27:05 +0900

    Update Video_Tools.js

--- Video_Tools.js
+++ Video_Tools.js
@@ -7,6 +7,8 @@
 // @exclude      *://challenges.cloudflare.com/*
 // @grant        none
 // @run-at       document-start
+// @updateURL    https://raw.githubusercontent.com/moamoa7/adblock/main/Video_Tools.js
+// @downloadURL  https://raw.githubusercontent.com/moamoa7/adblock/main/Video_Tools.js
 // ==/UserScript==
 
 (() => {
