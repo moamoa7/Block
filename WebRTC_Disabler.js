c59ec1a3804bafb1696681362c00697c74af246a
moamoa7 <chandung@nate.com>
2026-09-21 08:15:58 +0900

    Update WebRTC_Disabler.js

--- WebRTC_Disabler.js
+++ WebRTC_Disabler.js
@@ -14,8 +14,8 @@
 // @exclude      *://*.google.com/recaptcha/*
 // @exclude      *://www.recaptcha.net/*
 // @exclude      *://recaptcha.net/*
-// @updateURL    https://cdn.jsdelivr.net/gh/moamoa7/adblock@main/WebRTC_Disablerr.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/moamoa7/adblock@main/WebRTC_Disablerr.js
+// @updateURL    https://cdn.jsdelivr.net/gh/moamoa7/adblock@main/WebRTC_Disabler.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/moamoa7/adblock@main/WebRTC_Disabler.js
 // ==/UserScript==
 
 (() => {
