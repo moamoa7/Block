5ba9d5b9e12098a10edaf7a8d947e99fa3a16f05
moamoa7 <chandung@nate.com>
2026-09-21 08:21:36 +0900

    Update URLs for script updates and downloads

--- Web_Turbo.js
+++ Web_Turbo.js
@@ -15,8 +15,8 @@
 // @exclude      *://www.photopea.com/*
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://cdn.jsdelivr.net/gh/moamoa7/adblock@main/Web_Turbo.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/moamoa7/adblock@main/Web_Turbo.js
+// @updateURL    https://raw.githubusercontent.com/moamoa7/adblock/main/Web_Tubro.js
+// @downloadURL  https://raw.githubusercontent.com/moamoa7/adblock/main/Web_Tubro.js
 // ==/UserScript==
 
 'use strict';
